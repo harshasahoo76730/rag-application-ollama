4ef0851faac8939fd8c02050cc5b4d65b0a75a97
Harsha Vardhan Sahoo <harshasahoo76730@gmail.com>
2024-12-30 23:14:39 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # rag-development-environment
+
+## Steps to Use
+
+1.  **Set Up Ollama Requirements:**
+
+    - Follow the instructions at [Ollama Download](https://ollama.com/download) to download and install Ollama for your environment.
+    - After installing Ollama, setup the required models using the following commands:
+
+      ```sh
+      ollama run llama3.2
+      ollama run nomic-embed-text
+      ```
+
+    - Ensure you have the correct configuration settings for Ollama in your environment.
+
+2.  **Check Ollama Status:**
+
+    - Use the following `curl` command to check if Ollama is up and running:
+
+      ```sh
+      curl http://localhost:11434
+      ```
+
+    - You should see the below output:
+
+      ```sh
+      Ollama is running
+      ```
+
+3.  **Prepare the Environment:**
+
+    - Ensure you have all the necessary dependencies installed. You can typically do this by running:
+
+      ```sh
+      pip install -r requirements.txt
+      ```
+
+4.  **Populate the Database:**
+
+    - To add your own PDF files, place them in the `data` directory.
+    - To remove PDF files, simply delete them from the `data` directory.
+    - After adding or removing files, to populate the database with documents, run the `populate_database.py` script. This script will load documents from the `data` directory, split them into chunks, and add them to the Chroma database.
+
+      ```sh
+      python populate_database.py
+      ```
+
+    - If you want to reset the database before populating it, use the `--reset` flag:
+
+      ```sh
+      python populate_database.py --reset
+      ```
+
+5.  **Query the Database:**
+
+    - To query the database, run the `query_app.py` script with the query text as an argument.
+
+      ```sh
+      python query_app.py "Your query text here"
+      ```
---
+++ populate_database.py
@@ -0,0 +1,106 @@
+import os
+import shutil
+import argparse
+
+from get_embedding import get_embedding_function
+from langchain.schema.document import Document
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "data"
+
+
+def main():
+    # Check if the database should be cleared (using the --reset flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset Database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Resetting Database")
+        reset_database()
+
+    # Create (or update) the database.
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def add_to_chroma(chunks: list[Document]):
+    # Load the existing database.
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+
+    # Calculate Page IDs.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # Add (or update) the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # Add documents that don't exist in the DB.
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+    else:
+        print("✅ No new documents to add")
+
+
+def calculate_chunk_ids(chunks):
+    # Page Source : Page Number : Chunk Index
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # If the Page ID is the same as the last one, increment the index.
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        # Calculate the Chunk ID.
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # Add it to the page metadata.
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+
+def reset_database():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+
+if __name__ == "__main__":
+    main()
